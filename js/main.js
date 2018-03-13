944ea4a8efb30d5b256ecf6f4b462d0017be84fd
Salvador <chavatloz@gmail.com>
2018-03-12 23:53:41 -0600

    Reconocimiento de teclas y cambio de cordenadas en el evento

--- js/main.js
+++ js/main.js
@@ -79,21 +79,24 @@
     this.width = width;
     this.height = height;
     this.board = board;
-
     this.board.bars.push(this);
-
     this.kind = 'rectangle';
+    this.speed = 10;
 
   }
 
   self.Bar.prototype = {
     down: function() {
-
+      this.y += this.speed;
 
     },
 
     up: function() {
 
+      this.y -= this.speed;
+    },
+    toString: function(){
+      return 'x: '+ this.x +'y: '+this.y;
 
     }
 
@@ -103,18 +106,30 @@
 })();
 
 
+//Variables
+
+var board = new Board(800, 400);
+var bar = new Bar(20, 100, 30, 100, board);
+var bar2 = new Bar(750, 100, 30, 100, board);
+var canvas = document.getElementById('canvas');
+var view = new BoardView(canvas, board);
+
+//Eventos
+document.addEventListener('keydown',function(ev){
+  if (ev.keyCode == 38) {
+    bar.up();
+  }
+  else if (ev.keyCode == 40) {
+    bar.down();
+  }
+
+  console.log(bar.toString());
+
+});
+
 window.addEventListener('load', main);
 
 // Funcion principal
 function main() {
-
-  var board = new Board(800, 400);
-  var bar = new Bar(20, 100, 30, 100, board);
-  var bar2 = new Bar(750, 100, 30, 100, board);
-  console.log(bar2);
-  console.log(bar);
-  var canvas = document.getElementById('canvas');
-  var view = new BoardView(canvas, board);
-  console.log(view);
   view.draw();
 }
